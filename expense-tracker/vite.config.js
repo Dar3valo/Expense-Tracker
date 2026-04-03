977e63ded3594a0e1f5af6c4306978b80f87e7e6
dar3valo <wongdarren516@gmail.com>
2026-04-03 16:55:28 +0800

    base path set

--- expense-tracker/vite.config.js
+++ expense-tracker/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/Expense-Tracker/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Expense-Tracker/' : '/',
   plugins: [
     react(),
     tailwindcss(),
